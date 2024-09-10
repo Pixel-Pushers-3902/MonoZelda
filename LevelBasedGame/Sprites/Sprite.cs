41213f2f06284fa4c90a112a67ead5375890f1d9
Carson Miller <xsedim@gmail.com>
2024-09-10 19:14:23 -0400

    Added Sprite, SpriteDict, and SpriteDrawer class.

---
+++ LevelBasedGame/Sprites/Sprite.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace LevelBasedGame.Sprites {
+    public class Sprite
+    {
+        public enum AnchorType {
+            center,
+            topLeft
+        }
+
+        public Rectangle SourceRect { get; set; }
+        public int FrameCount { get; set; }
+        public float Size { get; set; }
+        public AnchorType Anchor { get; set; }
+        public bool Animating { get; set; }
+
+        public void Draw(SpriteBatch spriteBatch, GameTime gameTime, Texture2D texture) {
+
+        }
+    }
+}
---
+++ LevelBasedGame/Sprites/SpriteDict.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+
+namespace LevelBasedGame.Sprites {
+    internal class SpriteDict
+    {
+        public Point Position { get; set; }
+        public bool Enabled { get; set; }
+
+        private Texture2D texture;
+        private Dictionary<string, Sprite> dict = new();
+
+        public void Draw(SpriteBatch spriteBatch, GameTime gameTime) {
+
+        }
+    }
+}
