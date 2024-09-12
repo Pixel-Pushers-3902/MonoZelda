cf50f9e480306f1e118039d1df0710b5debc56fa
Robert Greenslade <greenslade.12@osu.edu>
2024-09-11 20:32:08 -0400

    Added all skeleton keyboard functionality code

---
+++ LevelBasedGame/Commands/BlockCycleCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Numerics;
+
+namespace LevelBasedGame.Commands;
+public class BlockCycleCommand : ICommand
+{
+    IController controller;
+    int cycleAddition;
+    public BlockCycleCommand(IController controller, int cycleAddition)
+    {
+        this.controller = controller;
+        this.cycleAddition = cycleAddition;
+    }
+
+    public GameState Execute()
+    {
+        // Apply cycle addition to block list
+        Debug.WriteLine("Block list cycling by " + cycleAddition);
+
+        // Keep GameState the same inside the controller
+        return controller.GameState;
+    }
+
+    public GameState UnExecute()
+    {
+        throw new NotImplementedException();
+    }
+}
--- LevelBasedGame/Controllers/KeyboardController.cs
+++ LevelBasedGame/Controllers/KeyboardController.cs
@@ -1,6 +1,8 @@
 ﻿using LevelBasedGame.Commands;
 using Microsoft.Xna.Framework.Input;
 using System;
+using System.Collections.Generic;
+using System.Numerics;
 
 namespace LevelBasedGame.Controllers;
 public class KeyboardController : IController
@@ -84,17 +86,87 @@ public class KeyboardController : IController
             }
 
             // Check for Player movement input
+            if (keyboardState.IsKeyDown(Keys.W))
+            {
+                // Player move forward command
+                ICommand playerMoveCommand = new PlayerMoveCommand(this, new Vector2(1, 0));
+                newState = playerMoveCommand.Execute();
+            }
+            else if (keyboardState.IsKeyDown(Keys.S))
+            {
+                // Player move backward command
+                ICommand playerMoveCommand = new PlayerMoveCommand(this, new Vector2(-1, 0));
+                newState = playerMoveCommand.Execute();
+            }
+            else if (keyboardState.IsKeyDown(Keys.D))
+            {
+                // Player move right command
+                ICommand playerMoveCommand = new PlayerMoveCommand(this, new Vector2(0, 1));
+                newState = playerMoveCommand.Execute();
+            }
+            else if (keyboardState.IsKeyDown(Keys.A))
+            {
+                // Player move left command
+                ICommand playerMoveCommand = new PlayerMoveCommand(this, new Vector2(0, -1));
+                newState = playerMoveCommand.Execute();
+            }
 
             // Check for Player attack input
+            if (keyboardState.IsKeyDown(Keys.Z))
+            {
+                // Player primary attack
+                ICommand playerAttackCommand = new PlayerAttackCommand(this, 1);
+                newState = playerAttackCommand.Execute();
+            }
+            else if (keyboardState.IsKeyDown(Keys.N))
+            {
+                // Player primary attack
+                ICommand playerAttackCommand = new PlayerAttackCommand(this, 2);
+                newState = playerAttackCommand.Execute();
+            }
 
             // Check for Player damage applied
+            if (keyboardState.IsKeyDown(Keys.E))
+            {
+                ICommand playerTakeDamageCommand = new PlayerTakeDamageCommand(this, 10);
+                newState = playerTakeDamageCommand.Execute();
+            }
 
             // Check for Block / Obstacle cycle input
+            if (keyboardState.IsKeyDown(Keys.Y))
+            {
+                ICommand blockCycleCommand = new BlockCycleCommand(this, 1);
+                newState = blockCycleCommand.Execute();
+            }
+            else if (keyboardState.IsKeyDown(Keys.T))
+            {
+                ICommand blockCycleCommand = new BlockCycleCommand(this, -1);
+                newState = blockCycleCommand.Execute();
+            }
 
             // Check for Item cycle input
+            if (keyboardState.IsKeyDown(Keys.I))
+            {
+                ICommand itemCycleCommand = new ItemCycleCommand(this, 1);
+                newState = itemCycleCommand.Execute();
+            }
+            else if (keyboardState.IsKeyDown(Keys.U))
+            {
+                ICommand itemCycleCommand = new ItemCycleCommand(this, -1);
+                newState = itemCycleCommand.Execute();
+            }
 
             // Check for Enemy / NPC cycle input
-
+            if (keyboardState.IsKeyDown(Keys.P))
+            {
+                ICommand enemyCycleCommand = new EnemyCycleCommand(this, 1);
+                newState = enemyCycleCommand.Execute();
+            }
+            else if (keyboardState.IsKeyDown(Keys.O))
+            {
+                ICommand enemyCycleCommand = new EnemyCycleCommand(this, -1);
+                newState = enemyCycleCommand.Execute();
+            }
         }
 
         // Setting new Game State of keyboard controller if needed
