32fd2addd7f1981a1bcf0e431d09098833f80860
xrs6969 <118093553+xrs6969@users.noreply.github.com>
2024-09-15 17:07:32 -0400

    added player movement and attacking fucntionaliy

---
+++ MonoZelda/StateMachine/PlayerStateMachine.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using PixelPushers.MonoZelda.Sprites;
+using PixelPushers.MonoZelda.Controllers;
+
+namespace PixelPushers.MonoZelda
+{
+    public class PlayerStateMachine
+    {
+        public enum Direction { Up, Down, Left, Right, None }
+        public Direction CurrentDirection { get; private set; } = Direction.None;
+        public bool IsAttacking { get; private set; } = false;
+
+        public void ChangeDirection(Direction newDirection)
+        {
+            CurrentDirection = newDirection;
+        }
+    }
+}
