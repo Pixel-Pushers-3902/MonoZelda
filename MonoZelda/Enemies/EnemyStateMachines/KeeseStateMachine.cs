4773229cad06bbd990045ced2a7410560b9412e3
Calvin Long <long.2108@buckeyemail.osu.edu>
2024-09-15 21:57:14 -0400

    Working prototype of moving "enemy" (using player sprite for now).

---
+++ MonoZelda/Enemies/EnemyStateMachines/KeeseStateMachine.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using PixelPushers.MonoZelda;
+using PixelPushers.MonoZelda.Sprites;
+
+namespace MonoZelda.Enemies.EnemyStateMachines
+{
+    public class KeeseStateMachine
+    {
+        public bool FacingUp { get; private set; } = true;
+        public bool FacingLeft { get; private set; } = true;
+        public bool IsAttacking { get; private set; } = true; //could make decorator
+
+        private readonly SpriteDict keeseSpriteDict; //set once keese sprite dict is implemented;
+        private Point position = new Point(100, 100);
+
+        public KeeseStateMachine(SpriteDict keeseSpriteDict)
+        {
+            this.keeseSpriteDict = keeseSpriteDict;
+
+            keeseSpriteDict.SetSprite("walk_right");
+        }
+
+        public void ChangeVertDirection()
+        {
+            FacingUp = !FacingUp;
+        }
+
+        public void ChangeHorDirection()
+        {
+            FacingLeft = !FacingLeft;
+        }
+
+        public static void Attack() //not used
+        {
+        }
+
+        public void Update()
+        {
+            if (FacingLeft)
+            {
+                position.X += 2;
+            }else
+            {
+                position.X -= 2;
+            }
+
+            if (FacingUp)
+            {
+                position.Y += 2;
+            }
+            else
+            {
+                position.Y -= 2;
+            }
+
+            keeseSpriteDict.Position = position;
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            keeseSpriteDict.Draw(spriteBatch, gameTime);
+        }
+    }
+}
---
+++ MonoZelda/Enemies/IEnemy.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoZelda.Enemies
+{
+    public interface IEnemy
+    {
+        public void ChangeVertDirection();
+
+        public void ChangeHorDirection();
+
+        public void Attack();
+
+        public void Update();
+
+        public void Draw(SpriteBatch spriteBatch, GameTime gameTime);
+    }
+}
