952361dbeb8c95eb443dc9b32965f6ba56dcda95
Om Kurkure <kurkureom@gmail.com>
2024-09-21 15:49:37 -0400

    More Changes to Item

--- MonoZelda/Items/Item.cs
+++ MonoZelda/Items/Item.cs
@@ -28,66 +28,16 @@ public class Item : IItem
 
 	public void SetSpriteItemDict(SpriteDict ItemDict,SpriteBatch spriteBatch,GameTime gameTime)
 	{
-	    if (currentItem == ItemList.Compass)
-		{
-			ItemDict.SetSprite("compass");
-		}
-		else if (currentItem == ItemList.Map)
-		{
-			ItemDict.SetSprite("map");
-		}
-		else if (currentItem == ItemList.Key)
-		{
-			ItemDict.SetSprite("key");
-		}
-		else if (currentItem == ItemList.HeartContainer)
-		{
-			ItemDict.SetSprite("heart_containter");
-		}
-		else if (currentItem == ItemList.TriforcePiece)
-		{
-			ItemDict.SetSprite("triforce_piece");
-		}
-		else if (currentItem == ItemList.Boomerang)
-		{
-			ItemDict.SetSprite("wooden_boomerang");
-		}
-        else if (currentItem == ItemList.Bow)
-        {
-            ItemDict.SetSprite("bow");
-        }
-        else if (currentItem == ItemList.Heart)
-        {
-            ItemDict.SetSprite("heart");
-        }
-        else if (currentItem == ItemList.Rupee)
-        {
-            ItemDict.SetSprite("rupee");
-        }
-        else if (currentItem == ItemList.Arrow)
-        {
-            ItemDict.SetSprite("arrow");
-        }
-        else if (currentItem == ItemList.Bomb)
-        {
-            ItemDict.SetSprite("bomb");
-        }
-        else if (currentItem == ItemList.Fairy)
-        {
-			ItemDict.SetSprite("fairy");
-		}
-		else if (currentItem == ItemList.Clock)
-		{
-		    ItemDict.SetSprite("clock");
-		}
-		else if (currentItem == ItemList.BlueCandle)
-		{
-			ItemDict.SetSprite("blue_candle");
-		}
-		else if (currentItem == ItemList.BluePotion)
-		{
-			ItemDict.SetSprite("blue_potion");
-		}
+		// storing current item as a string
+		string itemName = ItemDictionary.ItemPairs[currentItem];
+
+		// set sprite to be drawn
+		ItemDict.SetSprite(itemName);
+	}
+
+	void PlaceItem()
+	{
+		// To-do: add working code. Used to place items like bombs
 	}
 
 }
---
+++ MonoZelda/Items/SpawnBehavior.cs
@@ -0,0 +1,24 @@
+﻿using PixelPushers.MonoZelda;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoZelda.Items;
+
+public class SpawnBehaviour : ISpawn
+{
+    private ISpawn Item;
+
+    public SpawnBehaviour()
+    {
+        ISpawn item = null;
+    }
+
+    public void setSpawnBehaviour(ItemList currentItem)
+    {
+        // To-do (for future workings of game. Classes are not finalized)
+    }
+
+}
