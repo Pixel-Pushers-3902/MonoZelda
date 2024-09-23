e7230db5eb7a106e20b54547f0733591bd35c3d3
Om Kurkure <kurkureom@gmail.com>
2024-09-23 18:47:50 -0400

    Adding cycling functionality to items, similar to that of the blocks/tiles

---
+++ MonoZelda/Items/ItemType.cs
@@ -0,0 +1,20 @@
+﻿namespace PixelPushers.MonoZelda.Items;
+
+public enum ItemList
+{
+    compass,
+    paper,
+    key_0,
+    heartcontainer,
+    triforce,
+    boomerang,
+    bow,
+    heart_full,
+    rupee,
+    arrow,
+    bomb,
+    fairy,
+    timer,
+    candle_blue,
+    potion_blue,
+}
