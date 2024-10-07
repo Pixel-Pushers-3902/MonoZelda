1e437c73fba915f0506e7fb22dfda769f7e2fd74
xrs6969 <klasmeier.1@buckeyemail.osu.edu>
2024-10-06 21:21:47 -0400

    player hitbox test

---
+++ MonoZelda/Collidable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace MonoZelda
+{
+    public class Collidable
+    {
+        public Rectangle Bounds { get; set; }
+
+        public Collidable(Rectangle bounds)
+        {
+            Bounds = bounds;
+        }
+
+        public bool Intersects(Collidable other)
+        {
+            return Bounds.Intersects(other.Bounds);
+        }
+
+        public Rectangle GetIntersectionArea(Collidable other)
+        {
+            return Rectangle.Intersect(Bounds, other.Bounds);
+        }
+    }
+}
---
+++ MonoZelda/Link/PlayerCollision.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using PixelPushers.MonoZelda.Link;
+using System.Diagnostics;
+
+namespace MonoZelda.Link
+{
+    public class PlayerCollision
+    {
+        private Rectangle boundingBox;
+        private readonly int width;
+        private readonly int height;
+        private Player player;
+        private Vector2 playerPosition;
+
+
+        public PlayerCollision(Vector2 initialPosition)
+        {
+            this.width = 16;
+            this.height = 16;
+            Update(initialPosition);
+        }
+
+        public void Update(Vector2 playerPosition)
+        {
+            boundingBox.X = (int)playerPosition.X - width / 2;
+            boundingBox.Y = (int)playerPosition.Y - height / 2;
+
+        }
+
+        public Rectangle GetBoundingBox()
+        {
+            return boundingBox;
+        }
+        public bool Intersects(Rectangle otherBox)
+        {
+            return boundingBox.Intersects(otherBox);
+        }
+    }
+}
